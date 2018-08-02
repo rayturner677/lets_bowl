40bf3d3cbd97cceaa516e7839b8347f27d8d80db
rturner <rturner@basecampcodingacademy.org>
2018-08-02 09:53:32 -0500

    bowling1.0

--- bowling.py
+++ bowling.py
@@ -2,35 +2,23 @@ def main():
     game = []
     total_frames = []
     final = sum(total_frames)
-    while True:
+
+    for frame in range(1, 10):
+        print('frame', frame)
         turn = int(input('How many pins did you knock down?'))
         second_turn = int(input('How many pins did knock down this time?'))
-        if turn > 11:
-            for frame in range(1, 10):
-                print('frame', frame)
-                game.append([turn])
-                total = turn + second_turn
-                final = sum(total_frames)
-                print('frame', frame, ':', total)
-                print(game)
-            else:
-                print('invalid response')
 
-            if second_turn > 11:
-                for frame in range(1, 10):
-                    print('frame', frame)
-                    game.append([second_turn])
-                    total = turn + second_turn
-                    final = sum(total_frames)
-
-                    print('frame', frame, ':', total)
-                    print(game)
-                else:
-                    print('invalid response')
-
-                    total_frames.append(total)
-
-        print(final)
+        while turn < 11 and second_turn < 11:
+            game.append([turn, second_turn])
+            total = turn + second_turn
+            total_frames.append(total)
+            final = sum(total_frames)
+            print('frame', frame, ':', total)
+            print(game)
+            print(final)
+            break
+        else:
+            print('invalid response')
 
 
 if __name__ == '__main__':
